3d6ae5f21aa7ad9f8a320278880e5029ac268baa
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-18 11:21:25 -0700

    Initial work on making login persist on front end

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -5,7 +5,27 @@ import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore()
+  const sessionData = JSON.parse(localStorage.getItem("sessionData"));
+  const preloadedState = sessionData ? sessionData : {}
+  const store = configureStore(preloadedState);
+
+  store.subscribe(() => {
+    const userId = store.getState().session.id;
+    if (userId == null) {
+      localStorage.removeItem("sessionData");
+    } else {
+      const currentUser = store.getState().entities.users[userId];
+      localStorage.setItem("sessionData", JSON.stringify({
+        entities: {
+          users: { [currentUser.id]: currentUser },
+        },
+        session: {
+          id: currentUser.id
+        }
+      }));
+    }
+  });
+
   ReactDOM.render(<Root store={store} />, root);
 
   // tests
