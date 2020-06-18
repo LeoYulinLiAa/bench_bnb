822032a29648ca5daf8fcc6824c04f5d31764a4c
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-18 09:53:17 -0700

    Some cleanup after testing

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { signup, login } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -10,8 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<Root store={store} />, root);
 
   // tests
-  window.signup = signup
-  window.login = login
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 });
--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { login } from "../actions/session_actions";
+import { login, signup } from "../actions/session_actions";
 import { useDispatch } from "react-redux";
 
 /**
